152624bacddaaae900faea9ce5ba44fa2bce6c38
prachi4917 <ise2017003@iiita.ac.in>
2019-08-01 18:34:39 +0530

    Hello

---
+++ CharDemo.java
@@ -0,0 +1,10 @@
+class CharDemo{
+	static char ch1;
+	public static void main(String args[]){
+		char ch = 'T';
+		System.out.println("Char representation = " + ch);
+		System.out.println("Unicode value of T = " + (int)ch);
+		System.out.println("Size of character =" + Character.SIZE + "bits");
+		System.out.println("Default value of character = " + ch1);	
+	}
+}
